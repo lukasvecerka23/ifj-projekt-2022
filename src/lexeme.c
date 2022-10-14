b8a92409642f7607bf7f0172333bd99add65c691
jachymdolezal <jachym.dolezal@libertyaces.com>
2022-10-14 10:39:22 +0200

    fix clang-format

--- src/lexeme.c
+++ src/lexeme.c
@@ -8,8 +8,7 @@
 
 // TODO: dynamically allocated
 char string[200] = {0};
-char *strings = &string[0];
-
+char* strings = &string[0];
 
 /*
 // TODO:
@@ -18,36 +17,50 @@ char *strings = &string[0];
 */
 States FSM(States curr_state, char edge) {
     switch (curr_state) {
-    case ERROR:
-        fprintf(stderr,"token should have been created.");
-    case START:
-        if (edge == ')') return RPAR;
-        if (edge == '(') return LPAR;
-        if (edge == ',') return COMMA;
-        if (edge == '{') return LCURL;
-        if (edge == '}') return RCURL;
-        if (edge == ':') return COLON;
-        if (edge == ';') return SEMICOLON;
-        if (edge == '.') return DOT;
-        if (edge == '+') return PLUS;
-        if (edge == '-') return DASH;
-        if (edge == '/') return SLASH;
-        if (edge == '$') return VARID;
-        if (edge == '*') return MUL;
-        if (isalpha(edge) || edge == '_') return FUNCID;
-        if (isspace(edge)) return START;
-        // if (edge == '"') return L_STRING TODO
-        if (isdigit(edge)) return NUMBER;
-        return ERROR;
-    case RPAR:
-    case COMMA:
-    case LCURL:
-    case RCURL:
-    case LPAR:
-    default:
-        return ERROR;
-
-    
+        case ERROR:
+            fprintf(stderr, "token should have been created.");
+        case START:
+            if (edge == ')')
+                return RPAR;
+            if (edge == '(')
+                return LPAR;
+            if (edge == ',')
+                return COMMA;
+            if (edge == '{')
+                return LCURL;
+            if (edge == '}')
+                return RCURL;
+            if (edge == ':')
+                return COLON;
+            if (edge == ';')
+                return SEMICOLON;
+            if (edge == '.')
+                return DOT;
+            if (edge == '+')
+                return PLUS;
+            if (edge == '-')
+                return DASH;
+            if (edge == '/')
+                return SLASH;
+            if (edge == '$')
+                return VARID;
+            if (edge == '*')
+                return MUL;
+            if (isalpha(edge) || edge == '_')
+                return FUNCID;
+            if (isspace(edge))
+                return START;
+            // if (edge == '"') return L_STRING TODO
+            if (isdigit(edge))
+                return NUMBER;
+            return ERROR;
+        case RPAR:
+        case COMMA:
+        case LCURL:
+        case RCURL:
+        case LPAR:
+        default:
+            return ERROR;
     }
     return ERROR;
 }
@@ -57,38 +70,38 @@ TODO:
 - error handling
 - use token argument
 */
-lexeme create_lex(States final, char *token){
-    switch(final){
+lexeme create_lex(States final, char* token) {
+    switch (final) {
         case LPAR:
-            return (lexeme){.lex=L_LPAR};
+            return (lexeme){.lex = L_LPAR};
         case RPAR:
-            return (lexeme){.lex=L_RPAR};
+            return (lexeme){.lex = L_RPAR};
         case COMMA:
-            return (lexeme){.lex=L_COMMA};
+            return (lexeme){.lex = L_COMMA};
         case DOT:
-            return (lexeme){.lex=L_DOT};
+            return (lexeme){.lex = L_DOT};
         case LCURL:
-            return (lexeme){.lex=L_LCURL};
+            return (lexeme){.lex = L_LCURL};
         case RCURL:
-            return (lexeme){.lex=L_RCURL};
+            return (lexeme){.lex = L_RCURL};
         case SEMICOLON:
-            return (lexeme){.lex=L_SEMICOL};
+            return (lexeme){.lex = L_SEMICOL};
         case COLON:
-            return (lexeme){.lex=L_COLON};
+            return (lexeme){.lex = L_COLON};
         case MUL:
-            return (lexeme){.lex=L_MUL};
+            return (lexeme){.lex = L_MUL};
         case SLASH:
-            return (lexeme){.lex=L_SLASH};
+            return (lexeme){.lex = L_SLASH};
         case PLUS:
-            return (lexeme){.lex=L_PLUS};
+            return (lexeme){.lex = L_PLUS};
         case DASH:
-            return (lexeme){.lex=L_DASH};
+            return (lexeme){.lex = L_DASH};
         case FUNCID:
-            return (lexeme){.lex=L_FUNCID};
+            return (lexeme){.lex = L_FUNCID};
         case VARID:
-            return (lexeme){.lex=L_VARID};
+            return (lexeme){.lex = L_VARID};
         case NUMBER:
-            return (lexeme){.lex=L_NUMBER};
+            return (lexeme){.lex = L_NUMBER};
         case ERROR:
             error_exit("reached end of token");
     }
@@ -101,23 +114,24 @@ TODO:
 */
 lexeme get_lex_value() {
     States now = START;
-    char *lex_start = string;
+    char* lex_start = string;
     while (true) {
         char edge = getchar();
-        if (edge == EOF){
+        if (edge == EOF) {
             if (now == START) {
-                return (lexeme){.lex=LEOF};
+                return (lexeme){.lex = LEOF};
             }
             return create_lex(now, lex_start);
         }
         States next = FSM(now, edge);
-        if (next == ERROR){
+        if (next == ERROR) {
             ungetc(edge, stdin);
-            *(strings++) = '\0'; // remove just for testing should be implemented better
+            *(strings++) =
+                '\0';  // remove just for testing should be implemented better
             return create_lex(now, lex_start);
         }
         *(strings++) = edge;
-        if (next == START){
+        if (next == START) {
             strings = lex_start;
         }
         now = next;
@@ -130,8 +144,8 @@ TODO:
     - uploading var id, func id to symtable
     - sending tokens to syntax analyzer
 */
-char* print_lex(lexeme lex){
-    switch(lex.lex){
+char* print_lex(lexeme lex) {
+    switch (lex.lex) {
         case L_LPAR:
             return "(";
         case L_RPAR:
@@ -164,10 +178,10 @@ char* print_lex(lexeme lex){
             return "FUNCID";
         case L_DASH:
             return "-";
-        warning_msg("reached end of file \n");
+            warning_msg("reached end of file \n");
     }
-    warning_msg("token should have been printed (didnt you forget to add it print func)");
+    warning_msg(
+        "token should have been printed (didnt you forget to add it print "
+        "func)");
     return "ERROR";
 }
-
-
