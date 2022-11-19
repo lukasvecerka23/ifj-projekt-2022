43dcd235e34a3fe11fb222a620f42d210b2e6362
gzvvyx <nespora@protonmail.com>
2022-11-19 12:07:46 +0100

    int value replaced with token struct in AST

--- src/ast.c
+++ src/ast.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 void dispose(ast_node_t* tree) {
     if (tree != NULL) {
         dispose(tree->left);
@@ -14,18 +13,18 @@ void dispose(ast_node_t* tree) {
 }
 
 /*
-* MakeTree(o, a, b) vytvori novy uzel o
-* navaze leveho syna a, praveho syna b
-* a vrati ukazatel na uzel o
-*/
+ * MakeTree(o, a, b) vytvori novy uzel o
+ * navaze leveho syna a, praveho syna b
+ * a vrati ukazatel na uzel o
+ */
 
-ast_node_t* make_tree(char o, ast_node_t *a, ast_node_t *b){
+ast_node_t* make_tree(lexeme o, ast_node_t* a, ast_node_t* b) {
     ast_node_t* new = malloc(sizeof(struct ast_node));
-    if (new == NULL){
+    if (new == NULL) {
         exit(99);
-        //nevim presne jak toto osetrit
+        // nevim presne jak toto osetrit
     } else {
-        new->val = o;
+        new->token = o;
         new->left = a;
         new->right = b;
 
@@ -34,27 +33,23 @@ ast_node_t* make_tree(char o, ast_node_t *a, ast_node_t *b){
 }
 
 /*
-* MakeLeaf(i.a) vytvori novy uzel i.a
-* (i.a je adresa do tabulky symbolu)
-* a vrati ukazatel na tento uzel
-*
-* kde a.i je hodnota z tabulky symbolu
-*/
+ * MakeLeaf(i.a) vytvori novy uzel i.a
+ * (i.a je adresa do tabulky symbolu)
+ * a vrati ukazatel na tento uzel
+ *
+ * kde a.i je hodnota z tabulky symbolu
+ */
 
-ast_node_t* make_leaf(char ia){
+ast_node_t* make_leaf(lexeme ia) {
     ast_node_t* new = malloc(sizeof(struct ast_node));
-    if (new == NULL){
+    if (new == NULL) {
         exit(99);
-        //nevim presne jak toto osetrit
+        // nevim presne jak toto osetrit
     } else {
-        new->val = ia;
+        new->token = ia;
         new->left = NULL;
         new->right = NULL;
 
         return new;
     }
-}
-
-void print_node(ast_node_t *node) {
-  printf("[%c]", node->val);
 }
--- src/ast.h
+++ src/ast.h
@@ -2,25 +2,20 @@
 #define IFJ_AST_H
 
 #include <stdbool.h>
+#include "lexeme.h"
 
-// Uzol stromu
+
+// Tree node
 typedef struct ast_node {
-  char val;               // hodnota
-  struct ast_node *left;  // left child
-  struct ast_node *right; // right child
+    lexeme token;            // token struct
+    struct ast_node* left;   // left child
+    struct ast_node* right;  // right child
 } ast_node_t;
 
 // void init(ast_node_t *tree);
-void dispose(ast_node_t *tree);
+void dispose(ast_node_t* tree);
 
-ast_node_t* make_tree(char o, ast_node_t *a, ast_node_t *b);
-ast_node_t* make_leaf(char ia);
-
-void postorder(ast_node_t *tree);
-void preorder(ast_node_t *tree);
-void inorder(ast_node_t *tree);
-
-
-void print_node(ast_node_t *node);
+ast_node_t* make_tree(lexeme o, ast_node_t* a, ast_node_t* b);
+ast_node_t* make_leaf(lexeme ia);
 
 #endif
