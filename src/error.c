9b67a546e546c0a47ff8a7d0a1609776ecb93fae
Lukas Vecerka <vecerkalukas18@gmail.com>
2022-11-24 15:45:55 +0100

    refactor exit codes

--- src/error.c
+++ src/error.c
@@ -5,9 +5,12 @@
 
 void exit_program(int err_code, char* msg) {
     switch (err_code) {
-        case 51:
+        case 1:
+            fprintf(stderr, "lexical error: %s\n", msg);
+            exit(1);
+        case 2:
             fprintf(stderr, "syntax error: %s\n", msg);
-            exit(51);
+            exit(2);
         case 3:
             fprintf(stderr, "semantic error: %s\n", msg);
             exit(3);
@@ -17,6 +20,15 @@ void exit_program(int err_code, char* msg) {
         case 5:
             fprintf(stderr, "semantic error: %s\n", msg);
             exit(5);
+        case 6:
+            fprintf(stderr, "semantic error: %s\n", msg);
+            exit(6);
+        case 7:
+            fprintf(stderr, "semantic error: %s\n", msg);
+            exit(7);
+        case 8:
+            fprintf(stderr, "semantic error: %s\n", msg);
+            exit(8);
         case 99:
             fprintf(stderr, "internal error: %s\n", msg);
             exit(99);
