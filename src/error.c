528bbb99b97b6cee062a6173d196994c776fd7bf
Lukas Vecerka <vecerkalukas18@gmail.com>
2022-12-05 21:40:35 +0100

    add description to exit program error codes

--- src/error.c
+++ src/error.c
@@ -14,30 +14,41 @@
 void exit_program(int err_code, char* msg) {
     switch (err_code) {
         case 1:
+            // Error in lexical analysis (wrong structure of actual lexeme)
             fprintf(stderr, "lexical error: %s\n", msg);
             exit(1);
         case 2:
+            // Error in syntactic analysis, wrong program syntax, etc.
             fprintf(stderr, "syntax error: %s\n", msg);
             exit(2);
         case 3:
+            // Undefined function or function redefinition
             fprintf(stderr, "semantic error: %s\n", msg);
             exit(3);
         case 4:
+            // Wrong parameter count in function call, wrong return value of
+            // function
             fprintf(stderr, "semantic error: %s\n", msg);
             exit(4);
         case 5:
+            // Using of undefined variable
             fprintf(stderr, "semantic error: %s\n", msg);
             exit(5);
         case 6:
+            // Missing/Unexpected expression in function return
             fprintf(stderr, "semantic error: %s\n", msg);
             exit(6);
         case 7:
+            // Wrong type compatibility in arithmetic, string or relation
+            // expression
             fprintf(stderr, "semantic error: %s\n", msg);
             exit(7);
         case 8:
+            // Others semantic errors
             fprintf(stderr, "semantic error: %s\n", msg);
             exit(8);
         case 99:
+            // Internal error of compiler
             fprintf(stderr, "internal error: %s\n", msg);
             exit(99);
     }
